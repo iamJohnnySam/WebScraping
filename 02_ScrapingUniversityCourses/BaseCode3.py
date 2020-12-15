ae51e065ac5a4f8b2b9cef3e78b91f45fc6df7ae
iamJohnnySam <51484429+iamJohnnySam@users.noreply.github.com>
2020-12-15 09:28:29 +0530

    Create BaseCode3.py

---
+++ 02_ScrapingUniversityCourses/BaseCode3.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup as BS
+
+driver = webdriver.Chrome("chromedriver.exe")
+
+courses = []
+
+def getSite (name, web, tag, value="none"):
+    driver.get(web)
+    content = driver.page_source
+    soup = BS(content, features="lxml")
+    
+    if value == "none":
+        for val in soup.find_all(tag):
+            try:
+                courses.append([name,val.text,val.contents[0].get('href')])
+            except:
+                pass
+    else:
+        for val in soup.find_all(tag, class_=value):
+            try:
+                courses.append([name,val.text,val.contents[0].get('href')])
+            except:
+                pass
+        
+    
+getSite ("Mancheseter", "https://www.manchester.ac.uk/study/undergraduate/courses/2021/", "div", "title")
+getSite ("Cambridge", "https://www.undergraduate.study.cam.ac.uk/courses", "h4")
+getSite ("Cheseter", "https://www1.chester.ac.uk/course_atoz/52", "span", "table__primary")
+
+print(courses)
