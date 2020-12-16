1b94e3c5e6b9d420cd771c10b99a6c3c09ce39e2
iamJohnnySam <51484429+iamJohnnySam@users.noreply.github.com>
2020-12-16 09:59:05 +0530

    Update Final.py

--- 02_ScrapingUniversityCourses/Final.py
+++ 02_ScrapingUniversityCourses/Final.py
@@ -6,8 +6,11 @@ driver = webdriver.Chrome("chromedriver.exe")
 courses = []
 
 def formatVal (temp):
-    if "\xa0" in temp:
-        temp = temp.replace("\xa0"," ")
+    temp = temp.replace("\xa0"," ")
+    temp = temp.replace("<br>","")
+    temp = temp.replace("\n","")
+    temp = temp.replace("View course page for","")
+    temp = temp.replace("  ","")
     
     temp.strip()
     return temp
@@ -29,38 +32,43 @@ def getSite (name, web, tag, value="none", valType = "class"):
             try:
                 courses.append([name,formatVal(val.text),web+val.contents[0].get('href')])
             except:
-                pass
+                try:
+                    courses.append([name,formatVal(val.text),"Not Available"])
+                except:
+                    pass
             
     elif valType == "target":
         for val in soup.find_all(tag, target=value):
             try:
                 courses.append([name,formatVal(val.text),web+val.contents[0].get('href')])
             except:
-                pass
+                try:
+                    courses.append([name,formatVal(val.text),"Not Available"])
+                except:
+                    pass
             
     else:
         print("Error")
         
    
-getSite ("University of Mancheseter", "https://www.manchester.ac.uk/study/undergraduate/courses/2021/", "div", "title")
+getSite ("University of Mancheseter", "https://www.manchester.ac.uk/study/undergraduate/courses/2021", "div", "title")
 getSite ("University of Cambridge", "https://www.undergraduate.study.cam.ac.uk/courses", "h4")
 getSite ("University of Hertfordshire", "https://www.herts.ac.uk/international/new-international-students/intakes/january-2021-courses", "th")
 getSite ("University of Cheseter", "https://www1.chester.ac.uk/course_atoz/52", "span", "table__primary")
 getSite ("Bangor University", "https://www.bangor.ac.uk/international/courses/january-start", "li")
+getSite ("Ulster University", "https://www.ulster.ac.uk/courses?&f.Level_u%7CY=Postgraduate", "h3", "h2 bl")
+getSite ("Northumbria University - London", "https://london.northumbria.ac.uk/courses","a", "_blank", "target")
+getSite ("Northumbria University - Newcastle (Undergrad)", "https://www.northumbria.ac.uk/study-at-northumbria/our-courses/january-intake?ls=undergraduate#result-listing","h2")
+getSite ("Northumbria University - Newcastle (Postgrad)", "https://www.northumbria.ac.uk/study-at-northumbria/our-courses/january-intake?ls=postgraduate#result-listing","h2")
+getSite ("University of Roehampton","https://www.roehampton.ac.uk/courses", "li")
+getSite ("Solent University","https://www.solent.ac.uk/courses","h2","course-landing__label")
+getSite ("University of Essex","https://www.essex.ac.uk/postgraduate/masters/courses","div","subject__title")
 
-#getSite ("Ulster University", "https://www.ulster.ac.uk/courses?&f.Level_u%7CY=Postgraduate", "h3", "h2 bl")
-
-#getSite ("Northumbria University - London", "https://london.northumbria.ac.uk/courses","a", "_blank", "target")
-#getSite ("University of Roehampton",
-#getSite ("Solent University",
-#getSite ("University of Essex",
-
-print(courses)
-
+# print(courses)
 
 import csv
 
-with open('output.csv', 'w', newline='') as myfile:
+with open('output.csv', 'w', newline='', encoding="utf-8") as myfile:
     wr = csv.writer(myfile, delimiter=',')
     for a in courses:
         print(a)
