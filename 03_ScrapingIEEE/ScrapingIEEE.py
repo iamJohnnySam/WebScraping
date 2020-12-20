79bf9e85f63f0a23f47bddcf329ee0d7d42f0872
iamJohnnySam <51484429+iamJohnnySam@users.noreply.github.com>
2020-12-20 15:12:03 +0530

    Update ScrapingIEEE.py

--- 03_ScrapingIEEE/ScrapingIEEE.py
+++ 03_ScrapingIEEE/ScrapingIEEE.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup as BS
+import time
 
 driver = webdriver.Chrome("chromedriver.exe")
 
@@ -11,37 +12,35 @@ for x in inp:
     web = "https://ieeexplore.ieee.org/xpl/RecentIssue.jsp?punumber="+str(x)
     
     driver.get(web)
+    time.sleep (3)
     content = driver.page_source
     soup = BS(content, features="lxml")
     
-    try:
-        name = soup.find_all("h1")[0].text
-        
-        try:
-            impact = soup.find_all("a", class_="stats-jhp-impact-factor")[0].text
-            impact = impact.replace(" Impact Factor","")
-        except:
-            impact = "Not Found"
-        
-        try:
-            eigen = soup.find_all("a", class_="stats-jhp-eigenfactor")[0].text
-            eigen = eigen.replace(" Eigenfactor","")
-        except:
-            eigen = "Not Found"
-            
-        desc = soup.find_all("span", class_="aims-scope-container")[0].text
-        desc = desc.replace("  ","")
-        desc = desc.strip()
-
-        out.append([name, web, impact, eigen, desc])
-    except:
-        out.append(["Output error " + web])
+    name = soup.find_all("h1")[0].text
     
+    try:
+        impact = soup.find_all("a", class_="stats-jhp-impact-factor")[0].text
+        impact = impact.replace(" Impact Factor","")
+    except:
+        impact = "Not Found"
+    
+    try:
+        eigen = soup.find_all("a", class_="stats-jhp-eigenfactor")[0].text
+        eigen = eigen.replace(" Eigenfactor","")
+    except:
+        eigen = "Not Found"
+        
+    desc = soup.find_all("span", class_="aims-scope-container")[0].text
+    desc = desc.replace("  ","")
+    desc = desc.strip()
+
+    out.append([name, web, impact, eigen, desc])
+    
+    print(name + " - done")
     
 import csv
 
 with open('output.csv', 'w', newline='', encoding="utf-8") as myfile:
     wr = csv.writer(myfile, delimiter=',')
     for a in out:
-        print(a)
         wr.writerow(a)
