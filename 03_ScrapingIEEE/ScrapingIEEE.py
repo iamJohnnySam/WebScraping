56a6d4a3b1c6bb1e0a1e886dda10a47148abeef1
iamJohnnySam <51484429+iamJohnnySam@users.noreply.github.com>
2020-12-20 14:43:16 +0530

    Create ScrapingIEEE.py

---
+++ 03_ScrapingIEEE/ScrapingIEEE.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup as BS
+
+driver = webdriver.Chrome("chromedriver.exe")
+
+inp = [9424,8425306,7756,19,5326,7361,7083369,6221021,6221020,34,6221037,6287639,6221036,6221037,3468,7361]
+inp = list(dict.fromkeys(inp))
+out = []
+
+for x in inp:
+    web = "https://ieeexplore.ieee.org/xpl/RecentIssue.jsp?punumber="+str(x)
+    
+    driver.get(web)
+    content = driver.page_source
+    soup = BS(content, features="lxml")
+    
+    try:
+        name = soup.find_all("h1")[0].text
+        
+        try:
+            impact = soup.find_all("a", class_="stats-jhp-impact-factor")[0].text
+            impact = impact.replace(" Impact Factor","")
+        except:
+            impact = "Not Found"
+        
+        try:
+            eigen = soup.find_all("a", class_="stats-jhp-eigenfactor")[0].text
+            eigen = eigen.replace(" Eigenfactor","")
+        except:
+            eigen = "Not Found"
+            
+        desc = soup.find_all("span", class_="aims-scope-container")[0].text
+        desc = desc.replace("  ","")
+        desc = desc.strip()
+
+        out.append([name, web, impact, eigen, desc])
+    except:
+        out.append(["Output error " + web])
+    
+    
+import csv
+
+with open('output.csv', 'w', newline='', encoding="utf-8") as myfile:
+    wr = csv.writer(myfile, delimiter=',')
+    for a in out:
+        print(a)
+        wr.writerow(a)
