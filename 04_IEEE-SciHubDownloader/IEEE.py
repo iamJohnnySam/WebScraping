d5fd6b920715d52c579d32b98676e3f4b2fa5b75
iamJohnnySam <51484429+iamJohnnySam@users.noreply.github.com>
2020-12-21 08:40:33 +0530

    Update IEEE.py

--- 04_IEEE-SciHubDownloader/IEEE.py
+++ 04_IEEE-SciHubDownloader/IEEE.py
@@ -6,19 +6,28 @@ from bs4 import BeautifulSoup as BS
 
 chrome_options = Options()
 chrome_options.add_experimental_option('prefs', {
-    "download.default_directory": "C:\\Users\\iamJohnnySam\\Desktop",
+    "download.default_directory": "C:\\Users\\johns\\Desktop",
     "download.prompt_for_download": False,
     "download.directory_upgrade": True,
     "plugins.always_open_pdf_externally": True
+    # "plugins.plugins_list": [{"enabled": False, "name": "Chrome PDF Viewer"}], # Disable Chrome's PDF Viewer
+    # "download.extensions_to_open": "applications/pdf",
+    # "safebrowsing.enabled": False
     })
 
 import time
+from random import random
+from random import seed
+seed(1)
+
+from urllib.request import urlopen
 
 driver = webdriver.Chrome("chromedriver.exe")
 getter = webdriver.Chrome("chromedriver.exe", options=chrome_options)
 
 while True:
-    inp = input("Enter the page you want downloaded: ") 
+    # inp = input("Enter the page you want downloaded: ")
+    inp = "https://ieeexplore.ieee.org/document/4037142"
     
     if inp == "x":
         break
@@ -38,9 +47,27 @@ while True:
     
     getter.get(scihub)
     getter.find_element_by_name('request').click()
+    time.sleep(2*random())
     getter.find_element_by_name('request').send_keys(name)
+    time.sleep(2*random())
     getter.find_element_by_id('open').click()
-    # time.sleep(1)
-    # getter.find_element_by_id('open-button').click()
+    
+    soup2 = BS(getter.page_source, features="lxml")
+    for a in soup2.find_all("a"):
+        try:
+            b = a['onclick'].replace("location.href='","")
+            if ".pdf" in a:
+                b = b.replace("?download=true'","")
+                print (b)
+        except:
+            pass
+        
+        try:
+            PDF = urlopen(b)
+            with open(name+".pdf", 'wb') as file:
+                file.write(PDF.read())
+                file.close()
+        except:
+            getter.find_element_by_partial_link_text('save').click()
     
 print ("End")
